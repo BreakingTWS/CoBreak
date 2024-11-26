a0b70d967e7990bc47cb0cfa5860557b4780e5fe
BreakerTW Security <breaker@localhost.localdomain>
2024-11-26 15:47:10 -0500

    firt commit

--- ext/cobreak/cobreak_attack_wordlist.c
+++ ext/cobreak/cobreak_attack_wordlist.c
@@ -1,29 +1,6 @@
 #include<cobreak_attack_wordlist.h>
 
-VALUE mCoBreakAttackWordlist;
-VALUE cCoBreakAttackWordlistMD4;
-VALUE cCoBreakAttackWordlistMD5;
-VALUE cCoBreakAttackWordlistHALF_MD5;
-VALUE cCoBreakAttackWordlistSHA1;
-VALUE cCoBreakAttackWordlistDouble_SHA1;
-VALUE cCoBreakAttackWordlistSHA2_224;
-VALUE cCoBreakAttackWordlistSHA2_256;
-VALUE cCoBreakAttackWordlistSHA2_384;
-VALUE cCoBreakAttackWordlistSHA2_512;
-VALUE cCoBreakAttackWordlistSHA3_224;
-VALUE cCoBreakAttackWordlistSHA3_256;
-VALUE cCoBreakAttackWordlistSHA3_384;
-VALUE cCoBreakAttackWordlistSHA3_512;
-VALUE cCoBreakAttackWordlistRipemd_160;
-VALUE cCoBreakAttackWordlistTiger_160;
-VALUE cCoBreakAttackWordlistBlake2s_128;
-VALUE cCoBreakAttackWordlistBlake2s_160;
-VALUE cCoBreakAttackWordlistBlake2b_160;
-VALUE cCoBreakAttackWordlistBlake2s_224;
-VALUE cCoBreakAttackWordlistBlake2s_256;
-VALUE cCoBreakAttackWordlistBlake2b_256;
-VALUE cCoBreakAttackWordlistBlake2b_384;
-VALUE cCoBreakAttackWordlistBlake2b_512;
+
 
 #define BLOCK_SIZE 1024
 #define MAX_HASH_LENGTH_MD 16
--- ext/cobreak/cobreak_attack_wordlist.h
+++ ext/cobreak/cobreak_attack_wordlist.h
@@ -7,6 +7,30 @@
 //definition body for file <header>
 #include<gcrypt.h>
 
+VALUE mCoBreakAttackWordlist;
+VALUE cCoBreakAttackWordlistMD4;
+VALUE cCoBreakAttackWordlistMD5;
+VALUE cCoBreakAttackWordlistHALF_MD5;
+VALUE cCoBreakAttackWordlistSHA1;
+VALUE cCoBreakAttackWordlistDouble_SHA1;
+VALUE cCoBreakAttackWordlistSHA2_224;
+VALUE cCoBreakAttackWordlistSHA2_256;
+VALUE cCoBreakAttackWordlistSHA2_384;
+VALUE cCoBreakAttackWordlistSHA2_512;
+VALUE cCoBreakAttackWordlistSHA3_224;
+VALUE cCoBreakAttackWordlistSHA3_256;
+VALUE cCoBreakAttackWordlistSHA3_384;
+VALUE cCoBreakAttackWordlistSHA3_512;
+VALUE cCoBreakAttackWordlistRipemd_160;
+VALUE cCoBreakAttackWordlistTiger_160;
+VALUE cCoBreakAttackWordlistBlake2s_128;
+VALUE cCoBreakAttackWordlistBlake2s_160;
+VALUE cCoBreakAttackWordlistBlake2b_160;
+VALUE cCoBreakAttackWordlistBlake2s_224;
+VALUE cCoBreakAttackWordlistBlake2s_256;
+VALUE cCoBreakAttackWordlistBlake2b_256;
+VALUE cCoBreakAttackWordlistBlake2b_384;
+VALUE cCoBreakAttackWordlistBlake2b_512;
 
 //initialize function
 void init_cobreak_attack_wordlist();
