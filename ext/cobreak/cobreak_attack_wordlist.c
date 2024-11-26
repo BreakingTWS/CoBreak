0a17a8bebbfc415d638d229e1fa60fffbb1515b9
BreakerTW Security <breaker@localhost.localdomain>
2024-11-26 14:52:00 -0500

    firt commit

--- ext/cobreak/cobreak_attack_wordlist.c
+++ ext/cobreak/cobreak_attack_wordlist.c
@@ -18,6 +18,7 @@ VALUE cCoBreakAttackWordlistRipemd_160;
 VALUE cCoBreakAttackWordlistTiger_160;
 VALUE cCoBreakAttackWordlistBlake2s_128;
 VALUE cCoBreakAttackWordlistBlake2s_160;
+VALUE cCoBreakAttackWordlistBlake2b_160;
 
 #define BLOCK_SIZE 1024
 #define MAX_HASH_LENGTH_MD 16
@@ -1244,7 +1245,77 @@ VALUE attackwordlist_blake2s_160(VALUE self, VALUE hash, VALUE dictionary) {
     return found_password;
 }
 
+//Define Blake2b-160 Crack
+void calcular_blake2b_160(const char *cadena, unsigned char *hash) {
+    gcry_md_hd_t handle;
+    gcry_md_open(&handle, GCRY_MD_BLAKE2B_160, 0);
+    gcry_md_write(handle, cadena, strlen(cadena));
+    gcry_md_final(handle);
+    memcpy(hash, gcry_md_read(handle, GCRY_MD_BLAKE2B_160), 20);
+    gcry_md_close(handle);
+}
 
+int comparar_hashes_blake2b_160(const unsigned char *hash1, const unsigned char *hash2) {
+    return memcmp(hash1, hash2, 20) == 0;
+}
+
+void hex_a_hash_blake2b_160(const char *hex, unsigned char *hash) {
+    for (size_t i = 0; i < 20; i++) {
+        sscanf(hex + 2 * i, "%2hhx", &hash[i]);
+    }
+}
+
+VALUE attackwordlist_blake2b_160(VALUE self, VALUE hash, VALUE dictionary) {
+    FILE *archivo = fopen(StringValueCStr(dictionary), "r");
+    if (archivo == NULL) {
+        rb_raise(rb_eIOError, "Error al abrir el archivo de texto");
+    }
+
+    unsigned char hash_objetivo[20];
+    hex_a_hash_blake2b_160(StringValueCStr(hash), hash_objetivo);
+    
+    VALUE found_password = Qnil;
+    unsigned char hash_actual[20];
+    
+    char *lineas[BLOCK_SIZE];
+    for (size_t i = 0; i < BLOCK_SIZE; i++) {
+        lineas[i] = malloc(MAX_LINE_LENGTH * sizeof(char));
+        if (lineas[i] == NULL) {
+            fclose(archivo);
+            rb_raise(rb_eRuntimeError, "Error de asignación de memoria");
+        }
+    }
+
+    while (1) {
+        size_t count = 0;
+
+        for (size_t i = 0; i < BLOCK_SIZE && fgets(lineas[count], MAX_LINE_LENGTH, archivo); i++) {
+            lineas[count][strcspn(lineas[count], "\r\n")] = 0;
+            count++;
+        }
+
+        if (count == 0) {
+            break;
+        }
+
+        for (size_t i = 0; i < count; i++) {
+            calcular_blake2b_160(lineas[i], hash_actual);
+ 
+            if (comparar_hashes_blake2b_160(hash_actual, hash_objetivo)) {
+                if (found_password == Qnil) {
+                    found_password = rb_str_new_cstr(lineas[i]);
+                }
+            }
+        }
+    }
+
+    for (size_t i = 0; i < BLOCK_SIZE; i++) {
+        free(lineas[i]);
+    }
+    fclose(archivo);
+    
+    return found_password;
+}
 
 void init_cobreak_attack_wordlist() {
     //Define module AttackWordlist in mCoBreak
@@ -1317,4 +1388,8 @@ void init_cobreak_attack_wordlist() {
     //Define class Blake2s-128 for AttackWordlist
     cCoBreakAttackWordlistBlake2s_160 = rb_define_class_under(mCoBreakAttackWordlist, "BLAKE2S_160", rb_cObject);
     rb_define_singleton_method(cCoBreakAttackWordlistBlake2s_160, "crack", attackwordlist_blake2s_160, 2);
+
+    //Define class Blake2b-128 for AttackWordlist
+    cCoBreakAttackWordlistBlake2b_160 = rb_define_class_under(mCoBreakAttackWordlist, "BLAKE2B_160", rb_cObject);
+    rb_define_singleton_method(cCoBreakAttackWordlistBlake2b_160, "crack", attackwordlist_blake2b_160, 2);
 }
--- lib/cobreak/force.rb
+++ lib/cobreak/force.rb
@@ -106,6 +106,9 @@ class Forze_brute
     elsif (type_hash.downcase.eql?('blake2s-160'))
       forzebrute.time = Time.now
       @result = CoBreak::AttackWordlist::BLAKE2S_160.crack(hash_input, wordlist)
+    elsif (type_hash.downcase.eql?('blake2b-160'))
+      forzebrute.time = Time.now
+      @result = CoBreak::AttackWordlist::BLAKE2B_160.crack(hash_input, wordlist)
     end
 
     if !(result.nil?)
