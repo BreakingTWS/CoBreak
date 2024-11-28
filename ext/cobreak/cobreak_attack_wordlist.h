092d875aa0080df960e946f658ed6651a54886b8
BreakerTW Security <breaker@localhost.localdomain>
2024-11-28 08:24:32 -0500

    firt commit

--- ext/cobreak/cobreak_attack_wordlist.h
+++ ext/cobreak/cobreak_attack_wordlist.h
@@ -7,33 +7,7 @@
 //definition body for file <header>
 #include<gcrypt.h>
 
-VALUE mCoBreakAttackWordlist;
-VALUE cCoBreakAttackWordlistMD4;
-VALUE cCoBreakAttackWordlistMD5;
-VALUE cCoBreakAttackWordlistHALF_MD5;
-VALUE cCoBreakAttackWordlistSHA1;
-VALUE cCoBreakAttackWordlistDouble_SHA1;
-VALUE cCoBreakAttackWordlistSHA2_224;
-VALUE cCoBreakAttackWordlistSHA2_256;
-VALUE cCoBreakAttackWordlistSHA2_384;
-VALUE cCoBreakAttackWordlistSHA2_512;
-VALUE cCoBreakAttackWordlistSHA3_224;
-VALUE cCoBreakAttackWordlistSHA3_256;
-VALUE cCoBreakAttackWordlistSHA3_384;
-VALUE cCoBreakAttackWordlistSHA3_512;
-VALUE cCoBreakAttackWordlistRipemd_160;
-VALUE cCoBreakAttackWordlistTiger_160;
-VALUE cCoBreakAttackWordlistBlake2s_128;
-VALUE cCoBreakAttackWordlistBlake2s_160;
-VALUE cCoBreakAttackWordlistBlake2b_160;
-VALUE cCoBreakAttackWordlistBlake2s_224;
-VALUE cCoBreakAttackWordlistBlake2s_256;
-VALUE cCoBreakAttackWordlistBlake2b_256;
-VALUE cCoBreakAttackWordlistBlake2b_384;
-VALUE cCoBreakAttackWordlistBlake2b_512;
-VALUE cCoBreakAttackWordlistWhirlpool;
-VALUE cCoBreakAttackWordlistStribog256;
-VALUE cCoBreakAttackWordlistStribog512;
+
 
 //initialize function
 void init_cobreak_attack_wordlist();
