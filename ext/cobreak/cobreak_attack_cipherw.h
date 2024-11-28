3129ae5d6226345b8036fa52c2e2f0e6db9bd6ee
BreakerTW Security <breaker@localhost.localdomain>
2024-11-28 08:26:49 -0500

    firt commit

--- ext/cobreak/cobreak_attack_cipherw.h
+++ ext/cobreak/cobreak_attack_cipherw.h
@@ -1,9 +1,7 @@
 #ifndef COBREAK_ATTACK_CIPHERW_RUBY
 #define COBREAK_ATTACK_CIPHERW_RUBY
 
-//include library <cobreak_ruby.h> for project
-#include<cobreak_ruby.h>
-
+#include<gcrypt.h>
 int cobreak_attack_cipherw();
 
 #endif
